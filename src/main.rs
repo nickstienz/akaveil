34b3ba406c2feb1ec3eb80fb7ce528023351ac9b
Nicholas Stienz <nickstienz@gmail.com>
2024-03-10 17:36:09 -0500

    mlua working :D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use mlua::prelude::*;
+
+fn main() -> LuaResult<()> {
+    let lua = Lua::new();
+
+    let table = lua.create_table()?;
+    table.set(1, "one")?;
+    table.set(2, "two")?;
+    table.set(3, "three")?;
+
+    lua.globals().set("table", table)?;
+
+    lua.load(
+        r#"
+        for k,v in pairs(table) do
+            print(k,v)
+        end
+        "#,
+    )
+    .exec()?;
+
+    Ok(())
 }
